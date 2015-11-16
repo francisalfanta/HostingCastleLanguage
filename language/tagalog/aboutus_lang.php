b9abcb8b537f61286a8152ebf8f4ba79aa808ca8
roxanne-2015 <info@investindubai.com>
2015-11-16 17:43:59 +0400

    Tungkol Saamin-Careers

--- language/tagalog/aboutus_lang.php
+++ language/tagalog/aboutus_lang.php
@@ -14,14 +14,61 @@ upang madagdagan ang mga benta, mapahusay ang pagbebenta, at humusay ang serbisy
 Kami ay nagbibigay ng mahusay na serbisyo sa kliyente sa pamamagitan ng aming makabagong diskarte, 
 pagbibigay pansin sa mga detalye, mga ideyang matipid sa gastos, kakayahang tumugon sa mga hamon at nangangako ng kalidad.';
 
+
 $lang['aboutus_readmore'] 		= 'magbasa ng higit pa';
+
+
 $lang['aboutus_how_we_work']	= 'Paano Kami Magtrabaho';
-$lang['aboutus_how_we_work1'] 	= 'Hindi kami tinanggap upang lumikha ng isang bagay na ordinaryo. Kami ay tinanggap dahil kami ay nakikinig, 
-nagplaplano at baguhin nang lubusan ang mga kurso ng mga negosyo ng aming mga kliyente. 
-Ang pag-unlad ng mga proyekto ay itinuturing na kumpleto kapag ikaw ay lubusan nasiyahan sa mga resulta.';
-$lang['aboutus_how_we_work2']	= 'Kami ay nagaaral'
-$lang['aboutus_how_we_work3'] 	= 'Kinikilala naming kayo at ang iyong negosyo. Sinusuri namin ang bawat detalye na maaari niyong maibigay sa amin at mula doon 
-kami ay humuhugis ng impormasyon at ilapat ito sa web.'
-$lang['aboutus_how_we_work4'] 	= 'Istraktura at Istratehiya'
-$lang['aboutus_how_we_work5'] 	= 'Pagtukoy at pagdodokumento ng arkitektura at pangunahing punsyon. 
-Sa pamamagitan ng mga ito maaari naming mapanatili ang daloy ng iyong negosyo at g upang bumuo ng isang plano para sa gabay sa bahagi ng pagunlad.'
+$lang['aboutus_how_we_work_1'] 	= 'Hindi kami tinanggap upang lumikha ng isang bagay na ordinaryo. Kami ay tinanggap dahil kami ay nakikinig, 
+nagplaplano at baguhin nang lubusan ang mga kurso ng mga negosyo ng aming mga kliyente. Ang pag-unlad ng mga proyekto ay itinuturing na kumpleto kapag ikaw ay 
+lubusan nasiyahan sa mga resulta.';
+$lang['aboutus_how_we_work_2']	= 'Kami ay nagaaral';
+$lang['aboutus_how_we_work_3'] 	= 'Kinikilala naming kayo at ang iyong negosyo. Sinusuri namin ang bawat detalye na maaari niyong maibigay sa amin at mula doon 
+kami ay humuhugis ng impormasyon at ilapat ito sa web.';
+$lang['aboutus_how_we_work_4'] 	= 'Istraktura at Istratehiya';
+$lang['aboutus_how_we_work_5'] 	= 'Pagtukoy at pagdodokumento ng arkitektura at pangunahing punsyon. 
+Sa pamamagitan ng mga ito maaari naming mapanatili ang daloy ng iyong negosyo at g upang bumuo ng isang plano para sa gabay sa bahagi ng pagunlad.';
+$lang['aboutus_how_we_work_6'] 	= 'Disenyo at Pamamaraan';
+$lang['aboutus_how_we_work_7']	= 'Trinatrabaho namin ang tipograpiya at pamamaraan ng kulay na iyong ninanais na lumitaw sa web. 
+Mula sa mga pagpipilian at impormasyon na ipinagkatiwala sa amin mailalabas naming ang tatak na nakikita mo saiyong isip.';
+$lang['aboutus_how_we_work_8']	= 'Tumutugon na Pagkakalatag';
+$lang['aboutus_how_we_work_9']	= 'Isaalang-alang namin ang itsura at anyo ng proyekto sa ibat ibang mga screen (mobile, tablet, laptop at sa anumang teknolohiya ng website na maaring mabuksan). 
+Ang kaangkupan sa ibat ibang mga browser ay dapat na isinasaalang-alang.';
+$lang['aboutus_how_we_work_10']	= 'Pangunahing Pagunlad';
+$lang['aboutus_how_we_work_11']	= 'Magbubuo kami ng punsyon kasama ng natapos na pamamaraan ng iyong web.';
+$lang['aboutus_how_we_work_12']	= 'Kalidad';
+$lang['aboutus_how_we_work_13']	= 'Ginagawa namin sa kalidad ng kasiguruhan pamamaraan upang matiyak na ang site ay 100% libre mula sa mga isyu. 
+Binibigyang halaga namin ang kalidad sa bawat proyektong kami ay nakatugon.';
+$lang['aboutus_how_we_work_14']	= 'Pagsasanay';
+$lang['aboutus_how_we_work_15']	= 'Kapag naibigay na namin ang tapos na produkto, binibigay din namin sa inyo ang kaalaman upang makontrol at 
+pamahalaan ang iyong site.';
+
+
+$lang['aboutus_latest_news']	= 'Pinakabagong mga Balita';
+$lang['aboutus_latest_news_1']	= 'Agosto 05, 2015 Miyerkules, Aqary CRM';
+$lang['aboutus_latest_news_2']	= 'Kami ay nagbubukas para sa libreng subok.';
+
+
+$lang['aboutus_Careers']	= 'Propesyon';
+$lang['aboutus_Careers_1']	= 'Kami ay kasalukuyang naghahanap ng:';
+$lang['aboutus_Careers_2']	= 'Sales and Marketing Representative (2)';
+$lang['aboutus_Careers_3']	= 'Administrative Assistant (0)';
+$lang['aboutus_Careers_4']	= 'Kinakailangan';
+$lang['aboutus_Careers_5']	= 'Bihasa sa wikang Ingles.';
+$lang['aboutus_Careers_6']	= 'Higit sa isang taon na karanasan.';
+$lang['aboutus_Careers_7']	= 'Kinakailangang may lisensiya sa pagmamaneho sa UAE';
+$lang['aboutus_Careers_8']	= 'Marunong sa Microsoft Office';
+$lang['aboutus_Careers_9']	= 'May karanasan sa Real Estate dito sa Dubai ';
+$lang['aboutus_Careers_10']	= 'Mga Pakinabang';
+$lang['aboutus_Careers_11']	= 'Suweldo at komisyon'
+$lang['aboutus_Careers_12']	= 'Pahintulot sa pagtratrabaho para sa 2 taon';
+$lang['aboutus_Careers_13']	= 'Sim card galing sa kumpaniya';
+$lang['aboutus_Careers_14']	= 'Gastos sa kotse o gastos sa transportasyon';
+$lang['aboutus_Careers_15']	= 'LIbreng pagsasanay at pagsali sa may karanasang koponan';
+$lang['aboutus_Careers_16']	= 'Form para sa Pagaapply';
+$lang['aboutus_Careers_17']	= 'Pangalan';
+$lang['aboutus_Careers_18']	= 'Posisyon na Inaapplyan';
+$lang['aboutus_Careers_19']	= 'Numero ng Mobile';
+$lang['aboutus_Careers_20']	= 'Email';
+$lang['aboutus_Careers_21']	= 'Tungkol saiyong Sarili';
+$lang['aboutus_Careers_22']	= 'Ipasa';
