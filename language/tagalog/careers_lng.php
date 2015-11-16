366523dff3a0686018e7caecad409f145dcfcf03
roxanne-2015 <info@investindubai.com>
2015-11-16 18:27:28 +0400

    latest news

---
+++ language/tagalog/careers_lng.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+$lang['careers_title'] 			= 'Propesyon';
+
+// we are currently looking for title
+$lang['we_are_looking_title']	= 'Kami ay kasalukuyang naghahanap ng';
+$lang['we_are_looking_title_1'] = 'Sales and Marketing Representative (2)';
+$lang['we_are_looking_title_2'] = 'Administrative Assistant (0)';
+
+//Requirements
+$lang['req_title'] 				= 'Kinakailangan';
+$lang['req_title_1'] 			= 'Bihasa sa wikang Ingles.';
+$lang['req_title_2']			= 'Higit sa isang taon na karanasan.';
+$lang['req_title_3'] 			= 'Kinakailangang may lisensiya sa pagmamaneho sa UAE';
+$lang['req_title_4'] 			= 'Marunong sa Microsoft Office';
+$lang['req_title_5'] 			= 'May karanasan sa Real Estate dito sa Dubai ';
+
+//benefits
+$lang['benefits_title']			= 'Mga Pakinabang';
+$lang['benefits_title'] 		= 'Suweldo at komisyon';
+$lang['benefits_title'] 		= 'Pahintulot sa pagtratrabaho para sa 2 taon';
+$lang['benefits_title'] 		= 'Sim card galing sa kumpaniya';
+$lang['benefits_title'] 		= 'Gastos sa kotse o gastos sa transportasyon';
+$lang['benefits_title'] 		= 'Libreng pagsasanay at pagsali sa may karanasang koponan';
+
+
+//application form
+$lang['aplication_form_title'] 	= 'Form para sa Pagaapply';
+$lang['appl_name'] 			   	= 'Pangalan';
+$lang['appl_position']		   	= 'Posisyon na Inaapplyan';
+$lang['appl_mob_no']		   	= 'Numero ng Mobile';
+$lang['appl_email']			   	= 'Email';
+$lang['appl_about']			   	= 'Tungkol saiyong Sarili';
+$lang['appl_about']			   	= 'Ipasa';
---
+++ language/tagalog/latestnews_lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['latestnews_title'] 		= 'Pinakabagong mga Balita';
+$lang['latestnews_header_title']= 'Agosto 05, 2015 Miyerkules, Aqary CRM';
+$lang['latestnews_content'] 	= "Kami ay nagbubukas para sa libreng subok.";
