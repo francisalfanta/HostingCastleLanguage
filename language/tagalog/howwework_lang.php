f99fcfb2b0acbc55d8dca5dfbd177188ae9f1292
roxanne-2015 <info@investindubai.com>
2015-11-16 18:42:27 +0400

    How we work

---
+++ language/tagalog/howwework_lang.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['aboutus_how_we_work']	= 'Paano Kami Magtrabaho';
+$lang['aboutus_how_we_work_1'] 	= 'Hindi kami tinanggap upang lumikha ng isang bagay na ordinaryo. Kami ay tinanggap dahil kami ay nakikinig, 
+nagplaplano at baguhin nang lubusan ang mga kurso ng mga negosyo ng aming mga kliyente. Ang pag-unlad ng mga proyekto ay itinuturing na kumpleto kapag ikaw ay 
+lubusan nasiyahan sa mga resulta.';
+$lang['aboutus_how_we_work_2']	= 'Kami ay nagaaral';
+$lang['aboutus_how_we_work_3'] 	= 'Kinikilala naming kayo at ang iyong negosyo. Sinusuri namin ang bawat detalye na maaari niyong maibigay sa amin at mula doon 
+kami ay humuhugis ng impormasyon at ilapat ito sa web.';
+$lang['aboutus_how_we_work_4'] 	= 'Istraktura at Istratehiya';
+$lang['aboutus_how_we_work_5'] 	= 'Pagtukoy at pagdodokumento ng arkitektura at pangunahing punsyon. 
+Sa pamamagitan ng mga ito maaari naming mapanatili ang daloy ng iyong negosyo at g upang bumuo ng isang plano para sa gabay sa bahagi ng pagunlad.';
+$lang['aboutus_how_we_work_6'] 	= 'Disenyo at Pamamaraan';
+$lang['aboutus_how_we_work_7']	= 'Trinatrabaho namin ang tipograpiya at pamamaraan ng kulay na iyong ninanais na lumitaw sa web. 
+Mula sa mga pagpipilian at impormasyon na ipinagkatiwala sa amin mailalabas naming ang tatak na nakikita mo saiyong isip.';
+$lang['aboutus_how_we_work_8']	= 'Tumutugon na Pagkakalatag';
+$lang['aboutus_how_we_work_9']	= 'Isaalang-alang namin ang itsura at anyo ng proyekto sa ibat ibang mga screen (mobile, tablet, laptop at sa anumang teknolohiya ng website na maaring mabuksan). 
+Ang kaangkupan sa ibat ibang mga browser ay dapat na isinasaalang-alang.';
+$lang['aboutus_how_we_work_10']	= 'Pangunahing Pagunlad';
+$lang['aboutus_how_we_work_11']	= 'Magbubuo kami ng punsyon kasama ng natapos na pamamaraan ng iyong web.';
+$lang['aboutus_how_we_work_12']	= 'Kalidad';
+$lang['aboutus_how_we_work_13']	= 'Ginagawa namin sa kalidad ng kasiguruhan pamamaraan upang matiyak na ang site ay 100% libre mula sa mga isyu. 
+Binibigyang halaga namin ang kalidad sa bawat proyektong kami ay nakatugon.';
+$lang['aboutus_how_we_work_14']	= 'Pagsasanay';
+$lang['aboutus_how_we_work_15']	= 'Kapag naibigay na namin ang tapos na produkto, binibigay din namin sa inyo ang kaalaman upang makontrol at 
+pamahalaan ang iyong site.';
