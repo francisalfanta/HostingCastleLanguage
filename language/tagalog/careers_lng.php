d0c7a5a2b61d89d04215faea62ecb8dc46801609
roxanne-2015 <info@investindubai.com>
2015-11-16 18:29:42 +0400

    careers

--- language/tagalog/careers_lng.php
+++ language/tagalog/careers_lng.php
@@ -4,14 +4,15 @@
  *
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 $lang['careers_title'] 			= 'Propesyon';
 
-// we are currently looking for title
+// Kami ay kasalukuyang naghahanap ng
 $lang['we_are_looking_title']	= 'Kami ay kasalukuyang naghahanap ng';
 $lang['we_are_looking_title_1'] = 'Sales and Marketing Representative (2)';
 $lang['we_are_looking_title_2'] = 'Administrative Assistant (0)';
 
-//Requirements
+//Kinakailangan
 $lang['req_title'] 				= 'Kinakailangan';
 $lang['req_title_1'] 			= 'Bihasa sa wikang Ingles.';
 $lang['req_title_2']			= 'Higit sa isang taon na karanasan.';
@@ -19,7 +20,7 @@ $lang['req_title_3'] 			= 'Kinakailangang may lisensiya sa pagmamaneho sa UAE';
 $lang['req_title_4'] 			= 'Marunong sa Microsoft Office';
 $lang['req_title_5'] 			= 'May karanasan sa Real Estate dito sa Dubai ';
 
-//benefits
+//Mga Pakinabang
 $lang['benefits_title']			= 'Mga Pakinabang';
 $lang['benefits_title'] 		= 'Suweldo at komisyon';
 $lang['benefits_title'] 		= 'Pahintulot sa pagtratrabaho para sa 2 taon';
@@ -28,7 +29,7 @@ $lang['benefits_title'] 		= 'Gastos sa kotse o gastos sa transportasyon';
 $lang['benefits_title'] 		= 'Libreng pagsasanay at pagsali sa may karanasang koponan';
 
 
-//application form
+//Form para sa Pagaapply
 $lang['aplication_form_title'] 	= 'Form para sa Pagaapply';
 $lang['appl_name'] 			   	= 'Pangalan';
 $lang['appl_position']		   	= 'Posisyon na Inaapplyan';
