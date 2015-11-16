a8af97b2ed2e6408441100ebc9d7c0ef807ae148
roxanne-2015 <info@investindubai.com>
2015-11-16 16:09:09 +0400

    Tungkol Saamin

---
+++ language/tagalog/aboutus_lang.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$lang['aboutus_title'] 			= 'Tungkol Saamin';
+$lang['aboutus_content']		= 'Ang Hosting Castle ay tumutulong sa mga kumpanya upang mapalawak ang kita, mapalawak ang kasiyahan, 
+at mapalawak ang bawat araw. Kami ay gumagawa ng software para maabot ang mga pangangailangan, mga badyet at mabigyang daan sa mga pangangailangan ng mga 
+negosyante, maliliit at katamtamang mga negosyo at malalaking mga korporasyon. Ang aming kumpanya ay nagbibigay-daan sa lahat ng mga industriya at mga merkado 
+upang madagdagan ang mga benta, mapahusay ang pagbebenta, at humusay ang serbisyo sa kustomer habang nadadagdagan ang pagiging produktibo at kita. 
+Kami ay nagbibigay ng mahusay na serbisyo sa kliyente sa pamamagitan ng aming makabagong diskarte, 
+pagbibigay pansin sa mga detalye, mga ideyang matipid sa gastos, kakayahang tumugon sa mga hamon at nangangako ng kalidad.';
+
+$lang['aboutus_readmore'] 		= 'magbasa ng higit pa';
+$lang['aboutus_how_we_work']	= 'Paano Kami Magtrabaho';
+$lang['aboutus_how_we_work1'] 	= 'Hindi kami tinanggap upang lumikha ng isang bagay na ordinaryo. Kami ay tinanggap dahil kami ay nakikinig, 
+nagplaplano at baguhin nang lubusan ang mga kurso ng mga negosyo ng aming mga kliyente. 
+Ang pag-unlad ng mga proyekto ay itinuturing na kumpleto kapag ikaw ay lubusan nasiyahan sa mga resulta.';
+$lang['aboutus_how_we_work2']	= 'Kami ay nagaaral'
+$lang['aboutus_how_we_work3'] 	= 'Kinikilala naming kayo at ang iyong negosyo. Sinusuri namin ang bawat detalye na maaari niyong maibigay sa amin at mula doon 
+kami ay humuhugis ng impormasyon at ilapat ito sa web.'
+$lang['aboutus_how_we_work4'] 	= 'Istraktura at Istratehiya'
+$lang['aboutus_how_we_work5'] 	= 'Pagtukoy at pagdodokumento ng arkitektura at pangunahing punsyon. 
+Sa pamamagitan ng mga ito maaari naming mapanatili ang daloy ng iyong negosyo at g upang bumuo ng isang plano para sa gabay sa bahagi ng pagunlad.'
