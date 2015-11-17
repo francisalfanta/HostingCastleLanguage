2f05db215edc4464a959d1929273f0bae4bd0196
roxanne-2015 <info@investindubai.com>
2015-11-17 15:26:36 +0400

    date

---
+++ language/tagalog/date_lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['sunday'] = 'Linggo';
+$lang['monday']	= 'Lunes';
+$lang['tuesday']= 'Martes';
+$lang['wednesday']='Miyerkules';
+$lang['thursday'] = 'Huwebes';
+$lang['friday']  = 'Biyernes';
+$lang['saturday']= 'Sabado';
