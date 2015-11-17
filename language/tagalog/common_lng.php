b31c35e866b9ca6dc53a6b899a971ee471b4c89f
roxanne-2015 <info@investindubai.com>
2015-11-17 17:37:34 +0400

    Common

---
+++ language/tagalog/common_lng.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//side links - Our Company
+$lang['company_title'] = 'Ang Aming Kumpanya';
+$lang['company_aboutus'] = 'Tungkol sa Hosting Castle';
+$lang['company_team'] = 'Ang Aming Grupo';
+$lang['company_howwework'] = 'Pano Kami Magtrabaho';
+$lang['company_latestnews'] = 'Mga Pinakabagong Balita';
+$lang['company_careers'] = 'Karera';
+$lang['company_location'] = 'Lakasyon';
+$lang['company_contacts'] = 'Kontak';
+$lang['company_address']  = 'Lokasyon';
