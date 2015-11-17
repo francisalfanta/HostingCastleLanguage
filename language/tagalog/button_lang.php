3b41e1a26504c56e62788d12b8e2d66851a7d125
roxanne-2015 <info@investindubai.com>
2015-11-17 10:56:23 +0400

    button

---
+++ language/tagalog/button_lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$lang['btn_search']   	= 'Hanapin';
+$lang['btn_free']     	= 'Libre';
+$lang['btn_ordernow'] 	= 'Umorder na Ngayon';
+$lang['btn_send']       = 'Ipadala';
+$lang['btn_subscribe'] 	= 'Sumuskribi';
+$lang['btn_booknow']    = 'Naglaan na Ngayon';
+$lang['btn_submit']	  	= 'Ipasa';
+$lang['btn_live_preview'] = 'Pangkasalukuyan Prebyu ';
+$lang['btn_view']	  	= 'Tingnan';
+$lang['btn_order']	  	= 'Umorder';
+$lang['btn_close']		= 'Isara';
