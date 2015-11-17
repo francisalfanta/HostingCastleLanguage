b3464dda78f3182ed329d06ad38221b80e8bb308
roxanne-2015 <info@investindubai.com>
2015-11-17 10:33:20 +0400

    Footer

--- language/tagalog/footer_lang.php
+++ language/tagalog/footer_lang.php
@@ -6,39 +6,40 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 // Get Started
-$lang['getstarted_title'] = 'Get Started';
-$lang['getstarted_login'] = 'Login';
-$lang['getstarted_register'] = 'Register';
-$lang['getstarted_forgotpassword'] = 'Forgot Password?';
+$lang['getstarted_title'] = 'Magsimula';
+$lang['getstarted_login'] = 'Maglogin';
+$lang['getstarted_register'] = 'Magrehistto';
+$lang['getstarted_forgotpassword'] = 'Nakalimutan ang kontrasenyas?';
 
 // Products
-$lang['products_title']     = 'Products';
+$lang['products_title']     = 'Produkto';
 $lang['products_aqary']     = 'Aqary';
 $lang['products_tejary']    = 'Tojary';
 $lang['products_amaal']     = 'Amaal';
-$lang['products_pricelist'] = 'Price List';
+$lang['products_pricelist'] = 'Listahan ng Presyo';
 
 //Company
-$lang['company_title']       = 'Company';
-$lang['company_about']       = 'About Hosting Castle';
-$lang['company_team']        = 'Our Team';
-$lang['company_howwework']   = 'How We Work';
-$lang['company_lastestnews'] = 'Lastest News';
-$lang['company_careers']     = 'Careers';
+$lang['company_title']       = 'Kumpanya';
+$lang['company_about']       = 'Tungkol sa Hosting Castle';
+$lang['company_team']        = 'Ang Aming Gropu';
+$lang['company_howwework']   = 'Paano Kami Magtrabahp';
+$lang['company_lastestnews'] = 'Pinakabagong Balita';
+$lang['company_careers']     = 'Karera';
 
 //Earn With Us
-$lang['earn_title']         = 'Earn with Us';
-$lang['earn_partner']       = 'Become a Sales Partner';
-$lang['earn_affiliate']     = 'Become an Affiliate';
+$lang['earn_title']         = 'Kumita Saamin';
+$lang['earn_partner']       = 'Maging Isang Kasosyo sa Pagbebenta';
+$lang['earn_affiliate']     = 'Maging Kaanib';
 
 //Support
-$lang['support_title']            = 'Support';
-$lang['support_contact']          = 'Contact Us';
-$lang['support_knowledgebase']    = 'Knowledgebase';
-$lang['support_license']          = 'License Agreement';
-$lang['support_terms']            = 'Terms and Condition';
-$lang['support_privacy']          = 'Privacy Policy';
+$lang['support_title']            = 'Suporta';
+$lang['support_contact']          = 'Makipag-ugnayan Saamin';
+$lang['support_knowledgebase']    = 'Base ng Kaalaman';
+$lang['support_license']          = 'Lisensya ng Kasunduan';
+$lang['support_terms']            = 'Mga Tuntunin at Kondisyon';
+$lang['support_privacy']          = 'Patakaran sa Privacy';
 
 //Follow us
-$lang['followus'] 			= 'Follow Us';
+$lang['followus'] 			= 'Sundan Kami';
 $lang['email_news_letter']  ='Email Newsletter';
+$lang['email_news_letter']  ='sumuskribi';
