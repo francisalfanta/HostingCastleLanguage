dc0553f6fbf861a3cf12e754aeab5a83a85f78dd
roxanne-2015 <info@investindubai.com>
2015-11-17 14:19:48 +0400

    Amaal

---
+++ language/tagalog/amaal_page_lang.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// sidelinks
+$lang['amaal_sidelink_title_product_cat'] = 'Kategorya ng mga Produkto';
+$lang['online_interior']		= 'Online Interior';
+$lang['medical_clinic']			= 'Medical Clinic';		
+$lang['spa']					= 'Spa';		
+$lang['salon']					= 'Salon';
+
+//Amaal sidelink
+$lang['amaal_prof_service_sidelink_title'] = 'Propesyunal na Serbisyo ng Amaal';
+$lang['amaal_prof_service_sidelink_desc']  = 'Mula sa pasadyang disenyo at ng malawak na koleksyon ng mga tema , maaari mong piliin kung aling mga hitsura pinakamahusay na akma sa inyong negosyo. Sa Amaal maaari mong subaybayan at kontrolin ang iyong negosyo sa anumang oras at kahit saan ka man. Maaari kang mag-paskil ng mahusay na deal at itaguyod ang mga serbisyo sa hinaharap upang mapalakas ang iyong benta.';
+
+//side tags
+$lang['amaal_sidetag']	   		= 'Etiketa';
+$lang['service_tag']	  	    = 'Serbisyo';
+$lang['portal_tag']	  	        = 'Portal';
+$lang['onlineservice_tag']	    = 'Online na Serbisyo';
+$lang['greatdeals_tag']	  	    = 'Magandang Alok';
+$lang['promotions_tag']	        = 'Mga Promosyon';
+$lang['purchaseonline_tag']	    = 'Online na Pagbili';
+
+//amaal alert popup
+$lang['amaal_aler_popup_title']   = 'Alertong Impormasyon';
+$lang['amaal_aler_popup_content'] = 'Ang produktong ito ay hindi pa ipinagbibili, mangyaring makipag-ugnay
+                                      <a href="mailto:info@hostingcastle.com" style="color:blue; font-style:italic;">info@hostingcastle.com</a>
+                                       para sa higit pang mga detalye.</p>';
