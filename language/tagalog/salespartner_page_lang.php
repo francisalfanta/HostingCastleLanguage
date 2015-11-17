46602006c5f3eaec4f3876d551e2e80901cac230
roxanne-2015 <info@investindubai.com>
2015-11-17 14:31:24 +0400

    Sales Partner

---
+++ language/tagalog/salespartner_page_lang.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//content
+$lang['salespartner_main_desc']   		  = 'Kunin ang lahat ng kailangan mo upang mapalago ang iyong negosyo at maghatid ng kalidad ng mga serbisyo katulong ang Hosting Castle Partner Program.';
+
+
+//inside box content
+$lang['salespartner_content_title'] 	  = '3 Hakbang para maging Kasosyo ng Hosting Castle';
+$lang['salespartner_content_heading1']    = 'Gumawa ng account';
+$lang['salespartner_content_heading1_desc'] = 'Magsign up para makagawa ng account sa hostingcastle.com, kung ikaw ay mayroon ng isa, mangyaring mag-log in.';
+
+$lang['salespartner_content_heading2']     = 'Kumpletuhin ang Pahina ng Iyong Profile';
+$lang['salespartner_content_heading2_desc']= 'Simulan ang proseso ng aplikasyon sa pamamagitang ng paggawa ng pahina ng profile ng kumpanya. Punan ng mga kinakailangang detalye at i-upload ang mahalagang mga dokumento .';
+
+$lang['salespartner_content_heading3']     = 'Pagpapatunay';
+$lang['salespartner_content_heading3_desc']= 'Pagkatapos ng pagsusumite ng mga kinakailangang dokumento , ibe-verify ito sa aming departamento ng administrasyon at padadalhan ka ng kumpirmasyon.';
