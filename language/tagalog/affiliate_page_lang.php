4c7fc097f3ed2d9b7dec44592e108d2314c851e7
roxanne-2015 <info@investindubai.com>
2015-11-17 17:29:43 +0400

    Affiliate

---
+++ language/tagalog/affiliate_page_lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['affiliate_content'] = ' <p>Kung gusto mo ang mga produkto ng Hosting Castle, sabihin sa mundo ang tungkol dito at kumita ng mga komisyon kapalit nito.</p>
+		                         <p>Ang kaanib na kasosyo ng Hosting Castle ay pamamaraan na nagbibigay-daan sa iyo upang kumita ng komisyon sa mga mamimili sumangguni sa iyo sa aming web site kapag nag-sign up para sa isang subskripsyon sa CRM. </p>
+		                          <ul>
+					                 <li>Makakakuha ka ng 5% komisyon sa lahat ng mamimili na maisasangguni mo saamin</li>
+					                 <li>Maaari mong subaybayan ang bilang ng mga sign- ups sa pamamagitan ng aming kaanib na sistema</li>
+					                 <li>Magbibigay kami sa iyo ng lahat ng code na nagli-link at imahe</li>
+					                 <li>Hahawakan namin ang lahat ng mga pre at post na benta ng suporta  at paghahatid ng mga Produkto</li>
+		                          </ul>
+		                         <p>Magsign up na ngayon at maari ka ng kumita bukas.</p>';
