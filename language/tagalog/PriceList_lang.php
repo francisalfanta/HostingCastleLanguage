6ccef9f22c2e5a70965002b35eff119c8b8995fd
roxanne-2015 <info@investindubai.com>
2015-11-17 10:50:21 +0400

    Pricelist

---
+++ language/tagalog/PriceList_lang.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['price_discount_annual'] = '2 Buwang diskwento kada taon';
+$lang['price_discount_semiannual'] = '1 Buwang diskwento kada anim na taon';
+
+$lang['aqary_admin_panel']   = 'Namamahala ng CRM account panel';
+$lang['tojary_admin_panel']  = 'Namamahala ng panel';
+$lang['amaal_admin_panel']   = 'Namamahala ng panel';
+
+$lang['aqary_theme']        = 'Natatanging tema';
+$lang['tojary_theme']       = 'Natatanging tema';
+$lang['amaal_theme']        = 'Natatanging tema para sa lokasyon ng kliyente';
+
+$lang['aqary_theme']        = 'Libreng alternatibong domain';
+$lang['tojary_theme']       = 'Libreng alternatibong domain';
+$lang['amaal_theme']        = 'Libreng alternatibong domain';
