6fcbc62d9dd4c365963d0ead0fb917440c98e45a
roxanne-2015 <info@investindubai.com>
2015-11-17 10:14:42 +0400

    Header

---
+++ language/tagalog/Header_lang.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//header icons
+$lang['header_telephone'] 			= 'Telepono';
+$lang['header_livechat']  			= 'Live chat';
+//  added popup_Live chat
+$lang['livechat_popup']  			= 'paumanhin nakapatay ang aming linya, mangyaring magemail saamin!';
+$lang['livechat_popup_1']  			= 'Sa kasamaang palad,an gaming mga kawani sa Live Help ay hindi magagamit.
+Kung gusto mong makipag-ugnay sa amin, mangyaring punan ng iyong mga detalye sa ibaba:';
+$lang['livechat_popup_2']  			= 'Pangalan';
+$lang['livechat_popup_3']  			= 'Email';
+$lang['livechat_popup_4']  			= 'Mensahe';
+$lang['livechat_popup_5']  			= 'Kodigong Pananda';
+$lang['livechat_popup_6']  			= 'Magpadala ng kopya ng mensaheng ito sa iyong email address.';
+$lang['livechat_popup_7']  			= 'Ipadala ang mensahe';
+$lang['livechat_popup_8']  			= 'Gawin: maging totoo at gamitin ang iyong tunay na pangalan at email address';
+$lang['livechat_popup_9']  			= 'Wag gawin: maging negatibo at magpadala ng spam';
+
+$lang['header_language'] 			= 'Lengwahe';
+
+// header login,signup buttons
+$lang['login_btn']  		 		= 'Maglog-in';
+$lang['signup_btn']		 			= 'Magsign-up';
+
+//header login window
+$lang['login_heading']   			= 'Maglogin';
+$lang['login_popup']   				= 'Iyong email';
+$lang['login_popup'] 				= 'Iyong Kontrasenyas';
+$lang['login_popup_forgot_password_1']	= 'Nakalimutan ang kontrasenyas';
+$lang['login_popup_forgot_password_2']	= 'Nakalimutan ang kontrasenyas? ';
+$lang['login_popup_forgot_password_3']	= 'Ilagay ang iyong email at ang instruksyon ay ipapadala sayo.';
+$lang['login_popup_forgot_password)_4']	= 'Ipadala';
+$lang['login_popup_forgot_password_5']	= 'Bumalik sa pahina ng home';
+
+$lang['signin_btn_inner']			= 'Magsign In';
+$lang['signup_btn_inner']			= 'Magsign Up';
+$lang['social_login_heading']   	= 'Maglogin gamit ang';
+
+$lang['social_media_login_facebook']= 'Facebook';
+$lang['social_media_login_twitter'] = 'Twitter';
+$lang['social_media_login_google'] 	= 'Google';
+$lang['social_media_login_linkedin']= 'Linkedin';
+
+$lang['close_login_btn']			= 'Isara ang Log-in';
+
+//header signup window
+$lang['signup_heading']   			= 'Magsignup';
+$lang['signup_heading_1']   		= 'Pangalan';
+$lang['signup_heading_2']   			= 'Apelyido';
+$lang['signup_heading_3']   			= 'Email Address';
+$lang['signup_heading_4']   			= 'Kontrasenyas';
+$lang['signup_heading_5']   			= 'Itype muli ang kontrasenyas';
+
+$lang['social_signup_heading']   		= 'Magsignup gamit ang';
+$lang['social_media_signup_facebook']	= 'Facebook';
+$lang['social_media_signup_twitter'] 	= 'Twitter';
+$lang['social_media_signup_google'] 	= 'Google';
+$lang['social_media_signup_linkedin']	= 'Linkedin';
+
+$lang['close_signup_btn']			= 'Isara ang Sign-up';
