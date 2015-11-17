bde28f7f1b58d07f49c7277500c3ba2dda19cca1
roxanne-2015 <info@investindubai.com>
2015-11-17 10:18:38 +0400

    Search Domain

---
+++ language/tagalog/SearchDomain_lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+//search a domain
+$lang['search_domain_heading'] = 'Maghanap ng Domain';
+$lang['search_domain_heading'] = 'Hanapin';
