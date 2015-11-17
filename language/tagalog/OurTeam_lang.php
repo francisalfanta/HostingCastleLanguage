574cad2b422c7cb5a8217480b09da0b4d7e1ca4d
roxanne-2015 <info@investindubai.com>
2015-11-17 11:13:11 +0400

    Our Team

---
+++ language/tagalog/OurTeam_lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['team_title'] = 'Ang Aming Grupo';
+$lang['team_content'] = "Ang Hosting Castle ay bagong tatag(2015) na IT kumpanya na nakabase sa UAE. Ang aming kumpanya ay dalubhasa sa pagbibigay
+sa mga kliyente ng isang kumpletong pangkalahatang-ideya sa real estate, online shopping at mga serbisyo.
+Ang aming mga serbisyo ay ang pagdisenyo ng web, pagunlad pati na rin ang promosyon ng web. Gamit ang malikhain at makabagong mga gawa ng aming koponan na binubuo
+ng mga malikhain, mga dalubhasa, propesyonal at mga masisipag gumawa sa amin at dahil dito kami ay naging natatangi mula sa iba. " . ;
+$lang['team_content_ceo'] = 'Ang pagkakaroon ng taon ng karanasan sa pagpapatakbo ng isang negosyo, siya ang namamahala sa operasyon ng kumpanya upang matiyak ang produksyon at
+kakayahang kumita ng Hosting Castle.';
+$lang['team_content_sales'] = 'Sumali sa Hosting Castle na may karanasan sa serbisyo sa kustomer. Si Mustafa ay kasalukuyang namumuno sa pangangasiwa,
+pagmemerkado at mga pagbebenta sa Hosting Castle.';
