7d0532cafca2606f2c545cda6605164f7a5bae19
roxanne-2015 <info@investindubai.com>
2015-11-17 12:40:49 +0400

    aqary page

---
+++ language/tagalog/aqary_page_lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['crm_sidelink_title'] = 'CRM Real Estate';
+$lang['about_aqary']		= 'Tungkol sa Aqary';
+$lang['aqary_sidelink_pricelist'] = 'Listahan ng Presyo';
+$lang['aqary_sidelink_knowledgebase'] = 'Base ng Kaalaman';
+$lang['aqary_sidelink_latestnews'] = 'Pinakabagong mga Baliita';
+
+$lang['crm_for_sidelink_title'] = 'CRM para sa Real Estate';
+$lang['crm_for_sidelink_descr'] = 'Pamahalaan ang iyong negosyo sa real estate sa isang sentralisadong platform. Sa pamamagitan ng mahusay na mga kasangkapan at kapaki-pakinabang na mga tampok ng Aqary maaari mong patakbuhin ang iyong ari-arian ng maayos at mabisa.';
+
+//tags
+$lang['tags']				= 'Etiketa';
+$lang['real_estate']		= 'Real Estate';
+$lang['property_management']= 'Pamamahala ng Ari-arian';
+$lang['leads']				= 'Leads';
+$lang['apartment']			= 'Apartment';
+$lang['villa']				= 'Villa';
+$lang['listing']			= 'Mga Listahan';
+
+//tabs -product overview
+$lang['aqary_product_overview']   = 'Pangkalahatang-ideya ng Produkto';
+$lang['aqary_product_overview_title'] = 'Aqary Real Estate CRM';
+$lang['aqary_product_overview_descr'] = '<p>Hindi lang kami bumubuo ng sistema ng CRM -kami ay tumutulong sa mga kumpanyang tulad ng sa iyo upang maging kapakipakinabang . Ang AQARY CRM ay ang pinaka- kumpleto at madaling -gamitin na CRM software sa industriya ng real estate. Ginagawa naming itong madali upang mapalawak ang lead generation at  upang iklian cycles ng benta, makapagsara ng mas madaming deals at upang madagdagan ang kita , habang naghahatid ng natatanging serbisyo sa customer .</p>
+							             <p>Ang Aqary ay may kapaki-pakinabang na tampok upang ang ng iyong negosyo ay pamahalaan ng magaan. Mga filter para sa paghahanap ng mga yunit sa iyong mga listahan upang agad na makita kung ano ang iyong hinahanap ay ginawa upang magamit ng kapaki-pakinabang sa pamamagitan ng sistema upang makatipid ng oras . Ang pagbabayad at  talaan ng transaksyon ay nakatago at ligtas. Ang Aqary ay may nakalaang imbakan para sa mahalagang mga dokumento at mga imahe o anumang mga pag-upload. Ang Kalendaryo at alerto sa SMS para sa mga paalala at newsletter ay ilan sa maraming mga importanteng kahalagahan ng sistema.</p>
+							             <p>Kung nais mong tuklasin ang higit pa sa mga produktong ito , magrehistro na ngayon para sa aming libreng pagsubok.</p>';
+
+
+
+//tabs-Features
+$lang['features']	       		 = 'Tampok';
+$lang['fea_listings']	   		 = 'Listahan';
+$lang['fea_listings_descr'] 	 = 'Pamahalaan at uriin ang iyong mga yunit para sa pagbebenta at pagarkila ng mas mabilis';
+$lang['fea_contacts']	   		 = 'Ugnayan';.
+$lang['fea_contacts_descr'] 	 = 'Subaybayan ang mga talaan o mga detalye ng iyong mga kaugnayan sa isang lugar.';
+$lang['fea_generate_report']	 = 'Bumuo ng mga Ulat';
+$lang['fea_generate_report_descr'] = 'Sa isang pindot lamang ay maaari kang makabuo ng mga ulat at i-download ang mga mahahalagang impormasyon nang mabilis';
+$lang['fea_sms_alerts']			 = 'Alerto sa SMS';
+$lang['fea_sms_alert_descr']     = 'Ipadala ang iyong mga kaugnay at mga ahente ang mga paalala at mga balita sa anumang oras sa pamamagitan ng SMS.';
+$lang['fea_calender']		     = 'Kalendaryo';
+$lang['fea_calender_descr']      = 'Huwag palampasin ang mga mahalagang kaganapan. Ipaskil ang iyong trabaho, aktibidad at schedule ng pulong sa kalendaryo';
+$lang['fea_security']			 = 'Siguridad';
+$lang['fea_security_descr']   	 = 'Limitahan ang mga gumagamit upang makita lamang ang mga itinalagang mga ugnay at mga ari-arian.';
+$lang['fea_doc_media_sorage']    = 'Imbakan ng mga Dokumento at Medya)';
+$lang['fea_doc_media_sorage_decsr'] = 'Mag-upload ng mga imahe at iba pang mga dokumento at panatilihin itong naka-link sa listahan ng record para sa madaling pag-access sa 
+iyong mga file para sa paggamit sa ibang pagkakataon.';
+$lang['fea_trans_payment']       = 'Transakyon at Pagbabayad';
+$lang['fea_trans_payment_decsr'] = 'Manatiling nakasubaybay sa mga detalye ng kabayaran at mga transaksyon ng account mula sa isang sentralisadong platform.';
+$lang['fea_email_marketing']	 = 'Email Marketing';
+$lang['fea_email_marketing_decsr'] = 'Pamahalaan ang iyong listahan ng mga email at magpadala ng mga newsletter o mga kampanya sa iyong mga ugnay . ';
+
+//tabs-Live preview
+$lang['live_preview']			= 'Pangkasalukuyang Prebyu';
+$lang['live_preview_title']		= 'Pagpapakita ng Aqary CRM ';
+//tabs-themes
+$lang['themes']					= 'Tema'; 
+
+//themes-free themes
+$lang['free_themes_title']		= 'Libreng Tema';
+$lang['realia_descr']  			= 'Sa mga gumagamit ng  Realia  ay maaaring pamahalaan ang kanilang sariling mga listahan kahit na walang access sa admin dashboard. 
+Ang tema ay ganap scalable at maaari mo itong gamitin para sa mga malalaking portal ng real estate o mga maliliit na mga ahensiya sa pamamagtan lamang ng ilang mga pagpindot.';
+$lang['realsite_descr']         = 'Realsite ay isang nangungunang real estate WordPress na tema. Ito ay resulta ng mga taon ng pagpapaunlad sa real estate industry. 
+Ang tema ay ganap na sumasakop sa mga pangangailangan ng mga ahensiya ng real estate o mga portal .';
+$lang['top_producer_descr']		= 'May temang ito ng maraming mga pagpipilian para sa pagpapasadya, ngunit hindi masyadong maraming kung saan ito ay napakalaki.';
+
+
+//themes-Premium themes 
+$lang['premium_themes_title']	= 'Premium na Tema';
+$lang['quick_sale'] 			= 'Ang temang ito ay ginawa para sa parehong mga ahensya ng real estate at mga may-ari ng ari-arian. Para sa pagbebenta ng may-ari ay  mas popular sa panahong ito
+at ang propesyonal na tema sa real estate ay espesyal na ginawa para sa mga naturang website. Maaari mo itong gamitin upang magbenta ng solong pag-aari object o baguhin ito bilang isang corporate na pahina para sa mga
+real estate na ahensiya. Kami ay sigurado na ang tema ay mapupunta lampas sa saklaw ng iyong paunang ekspektasyon at magbibigay ng mas maraming mga tampok kaysa 
+sa maaari mong isipin.';
+$lang['reales_wp']				= 'Ang temang ito ay moderno, malinis, madaling gamitin , at ganap na tumutugon na disenyo, na mukhang perpekto sa anumang laki ng tablet o 
+mobile screen.';
+$lang['home_town']				= 'Ang temang ito ay may mga elegante at malinis na disenyo at may kasamang maraming mga tampok tulad ng paghahanap ng ari-arian , slider ng ari-arian, mapa ngari-arian na may
+maistilong mga tanda , front end user login  at magparehistro, front end ng ari-arian sa pagsumite at pagbago, integrasyon sa pagbabayad gamit ang PayPal , pag dsIDXpress IDX Plugin, 
+WPML plugin suporta, mga pasadyang widget , ibat-ibang mga template, mahusay na dinisenyo mga pagpipilian sa tema at mga meta box, sa suporta ng Google reCAPTCHA at marami pang iba';
+$lang['solus_reales']			= 'Ang temang ito ay na-optimista para sa mataas na resolution ng display tulad ng Macbook Pro na may Retina Display, iPhone at iPad. Binuo gamit
+ang HTML5 boilerplate para sa isang mabilis, matatag at future proof site!';
+$lang['home_land']				= 'Ang isang mahusay na paghahanap at madaling gamitin na disenyo para sa pagpapakita ng isang propesyonal na imahe sa iyong mga bisita at potensyal na kliyente.
+Perpektong solusyon para sa mga propesyonal at mga ahensya ng nagtatrabaho sa mga domain ng Real Estate. Ito ay nagpapalawak nito na mas maabot ang mga target na madla 
+at nagdadala sa mas maraming mga kita.';
+$lang['real_expert']			= 'Ito ay may maganda, malinis , minimal na disenyo at ganap na tumutugon. Ito ay may maraming mga tampok tulad ng: integrasyon sa dsIDXpress plugin,
+maagang paghahanap ng ari-arian , default at grid contextual ,  slider na nilalaman ng ari-arian, metaboxes ng ari-arian, mga pasadyang widget , ibat-ibang mga template ng pahina , 
+mga pagpipilian sa tema, walang limitasyongkulay, walang limitasyong mga font at tonelada higit pa.';
+
+//tab-order now
+$lang['order_now']				= 'Maglaan na Ngayon';
+$lang['order_now_title']        = 'Maglaan';
