5cabcdcd168597108c60160c2df4eacfdb8e73a7
roxanne-2015 <info@investindubai.com>
2015-11-17 10:41:47 +0400

    Contents

---
+++ language/tagalog/content_lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['content_menu_home'] = 'Home';
+$lang['content_menu_aboutus'] = 'Tumgkol Saamin';
+$lang['content_menu_pricelist'] = 'Listahan ng Presyo';
+$lang['content_menu_aqary'] = 'Aqary';
+$lang['content_menu_tejary'] = 'Tojary';
+$lang['content_menu_amaal'] = 'Amaal';
+$lang['content_menu_knowledgebase'] = 'Base ng Kaalaman';
+$lang['content_menu_freetraining'] = 'Libreng Pagsasanay';
+$lang['content_menu_contact'] = 'Makipagugnayan';
+
+// Added by FMA 2015-11-08
+$lang['content_menu_login'] = 'Maglogin';
+$lang['content_menu_signup'] = 'Magsign Up';
