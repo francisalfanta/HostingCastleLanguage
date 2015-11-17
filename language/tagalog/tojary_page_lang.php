6d4e92ddb5ee4df60d4a26bd6010c54842acefcc
roxanne-2015 <info@investindubai.com>
2015-11-17 14:04:57 +0400

    Tojary Page

---
+++ language/tagalog/tojary_page_lang.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+//sidelinks
+$lang['tojary_sidelink_title_product_cat'] = 'Kategorya ng Produkto';
+$lang['apparel_shop']		   = 'Tindahan ng Damit'; 
+$lang['bedding_store']		   = 'Tindahan ng Gamit sa Pagtulog';
+$lang['book_shop']		   	   = 'Tindahan ng Libro';	
+$lang['flower_shop']		   = 'Tindahan ng Bulaklak';
+$lang['furniture_shop']		   = 'Tindahan ng Kasangkapan sa Bahay';
+$lang['handicraft']			   = 'Productong Gawa ng Kamay';
+$lang['home_decor_shop']	   = 'Tindahan ng Palamuti sa Bahay';
+$lang['jewelery_shop']		   = 'Tindahan ng Alahas';
+$lang['kids_store']		  	   = 'Tindahan ng Pambata';
+$lang['kitchen_shop']		   = 'Tindahan ng Kasangkapan sa Kusina';
+$lang['lamp_store']		   	   = 'Tindahan ng Lampara';
+$lang['medical_shop']		   = 'tindahan ng Gamot';
+$lang['online_food_order']	   = 'Online na Pagorder ng Pagkain';
+$lang['online_supermarket']	   = 'Online na Bilihan';
+$lang['online_tshirt_designer']= 'Online na PAgdedesenyo ng Damit';
+$lang['shoes_shop']		 	   = 'Tindahan ng Sapatos';
+$lang['sports_appareals_shop'] = 'Tindahan ng Pangstorts na Damit';
+$lang['tea_coffee_shop']	   = 'Tindahan ng KApe at Tsaa';
+$lang['watch_shop']		 	   = 'Tindahan ng Relo';
+$lang['womens_fashion_shop']   = 'Tindahan na Pambabae';
+		
+//tojary sidelink
+
+$lang['tojary_sidelink_title_ecommerce'] = 'Tojary E-komersyal na Shopping Cart na Solusyon';
+$lang['tojary_sidelink_desc']	= 'Ang Tojary ay nag-aalok ng malawak na seleksyon ng mga disenyo na naaangkop sa uri ng iyong negosyo . Hinahayaan ka nitong ipasadya ang istraktura ng iyong online na tindahan sa iyong sariliat sa paraan na gusto mo. Sa pagproseso ng mga bagong order at checkouts ay 100% ligtas sa loob ng sistema. Dahil sa madaling gamitin na interface ng Tojary , ang pagsubaybay sa iyong mga benta at imbentaryo ng mga produkto ay magiging simple at mas mababasawasan ang pagkabigo.';
+
+//side tags
+$lang['tojary_sidetag']	   		= 'Etiketa';
+$lang['products_tag']	  	    = 'Mga Produkto';
+$lang['onlineshop_tag']	        = 'Online na Tindahan';
+$lang['shop_tag']	  	   	    = 'Tindahan';
+$lang['payonline_tag']	        = 'Magbayad Online';
+$lang['shopthemes_tag']	  	    = 'Tema ng Tindahan';
+$lang['sale_tag']	            = 'Sale';
+
+//tojary alert popup
+$lang['tojary_aler_popup_title']   = 'Alertong Impormasyon';
+$lang['tojary_aler_popup_content'] = 'Ang produktong ito ay hindi pa ipinagbibili, mangyaring makipag-ugnay
+                                      <a href="mailto:info@hostingcastle.com" style="color:blue; font-style:italic;">info@hostingcastle.com</a>
+                                       para sa higit pang mga detalye.</p>';
+
+
+
+
+
+
+
+
+
+
+
+		
+		
+		
+                       
