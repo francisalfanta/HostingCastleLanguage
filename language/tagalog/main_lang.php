8e9e613afe920c09c898413f87a6d34a76128dcc
roxanne-2015 <info@investindubai.com>
2015-11-17 15:22:26 +0400

    main

---
+++ language/tagalog/main_lang.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// About Us
+$lang['main_aboutus_title'] = 'Tungkol Saamin';
+$lang['main_aboutus_content'] = ' tumutulong sa mga kumpanya upang mapalawak ang kita, mapalawak ang kasiyahan, at mapalawak ang bawat araw. Kami ay gumagawa ng software para maabot ang mga pangangailangan, mga badyet at mabigyang daan sa mga pangangailangan ng mga negosyante, maliliit at katamtamang mga negosyo at malalaking mga korporasyon. Ang aming kumpanya ay nagbibigay-daan sa lahat ng mga industriya at mga merkado 
+upang madagdagan ang mga benta, mapahusay ang pagbebenta, at humusay ang serbisyo sa kustomer habang nadadagdagan ang pagiging produktibo at kita. Kami ay nagbibigay ng mahusay na serbisyo sa kliyente sa pamamagitan ng aming makabagong diskarte, pagbibigay pansin sa mga detalye, mga ideyang matipid sa gastos, kakayahang tumugon sa mga hamon at nangangako ng kalidad.';
+$lang['main_aboutus_readmore'] = 'magbasa ng higit pa';
+$lang['main_aboutus_company_name'] = 'Hosting Castle';
+//Our Team
+$lang['main_team_title'] = 'Ang Aming Grupo';
+$lang['main_team_content_ceo'] = 'Ang pagkakaroon ng taon ng karanasan sa pagpapatakbo ng isang negosyo, siya ang namamahala sa operasyon ng kumpanya upang matiyak ang produksyon at kakayahang kumita ng Hosting Castle.';
+$lang['main_team_content_sales'] = 'Sumali sa Hosting Castle na may karanasan sa serbisyo sa kustomer. Si Mustafa ay kasalukuyang namumuno sa pangangasiwa, pagmemerkado at mga pagbebenta sa Hosting Castle.';
+
+//Products
+$lang['main_prod_aqary_header']  = 'Aqary';
+$lang['main_prod_aqary_title']   = 'Software ng Aqary Real Estate CRM';
+$lang['main_prod_aqary_content'] = 'Hindi lang kami bumubuo ng sistema ng CRM -kami ay tumutulong sa mga kumpanyang tulad ng sa iyo upang maging kapakipakinabang . Ang AQARY CRM ay ang pinaka- kumpleto at madaling -gamitin na CRM software sa industriya ng real estate. Ginagawa naming itong madali upang mapalawak ang lead generation at  upang iklian cycles ng benta, makapagsara ng mas madaming deals at upang madagdagan ang kita , habang naghahatid ng natatanging serbisyo sa customer .Ang Aqary ay may kapaki-pakinabang na tampok upang ang ng iyong negosyo ay pamahalaan ng magaan. Mga filter para sa paghahanap ng mga yunit sa iyong mga listahan upang agad na makita kung ano ang iyong hinahanap ay ginawa upang magamit ng kapaki-pakinabang sa pamamagitan ng sistema upang makatipid ng oras . Ang pagbabayad at  talaan ng transaksyon ay nakatago at ligtas. Ang Aqary ay may nakalaang imbakan para sa mahalagang mga dokumento at mga imahe o anumang mga pag-upload. 
+
+Ang Kalendaryo at alerto sa SMS para sa mga paalala at newsletter ay ilan sa maraming mga importanteng kahalagahan ng sistema.
+
+Kung nais mong tuklasin ang higit pa sa mga produktong ito , magrehistro na ngayon para sa aming libreng pagsubok.';
+
+$lang['main_aqary_readmore'] = 'magbasa ng higit pa';
+
+$lang['main_prod_tejary_header']  = 'Tojary';
+$lang['main_prod_tejary_title']   = 'Tojary E-Commercial shopping cart solution';
+$lang['main_prod_tejary_content'] = 'Upang maging matagumpay ngayon, ang kailangan mong ma-ibenta ang inyong mga produkto sa ibat ibang mga paraan hanggat maaari. Ipinapakita ng merkado na ang e- commerce ay may napakalaking epekto sa ibat ibang uri ng negosyo sa ngayon. Ngunit ano nga ba e -commerce? Ang E- commerce ay ang makabagong mekanismo na ginagamit upang ilarawan ang mga negosyo na itinatag sa internet.
+
+Sa pagpapakita ng iyong mga produkto sa online at ang pagkakaroon ng bilyon-bilyong mga manonood ito ay mukang nakakakumbinse ngunit sa paggawa ng mapaglinlang na trabahong ito ay nakakabigo. Wag ng mag-alala, ang paglagay nito sa online at nagbebenta ng iyong mga produkto sa buong mundo ay ginawa ng mas madali ngayon sa pamamagitan Tojary.
+
+Ang Tojary ay nag-aalok ng malawak na seleksyon ng mga disenyo na naaangkop sa uri ng iyong negosyo . Hinahayaan ka nitong ipasadya ang istraktura ng iyong online na tindahan sa iyong sariliat sa paraan na gusto mo. Sa pagproseso ng mga bagong order at checkouts ay 100% ligtas sa loob ng sistema. Dahil sa madaling gamitin na interface ng Tojary , ang pagsubaybay sa iyong mga benta at imbentaryo ng mga produkto ay magiging simple at mas mababasawasan ang pagkabigo.
+
+Kailangan mo lamang irehistro ang iyong negosyo sa amin at ang aming grupo ay gagawin ang lahat ng trabaho para sa iyo. Sa saglit na oras lamang ang iyong negosyo ay garantisadong magiging handa at makikita na.';
+$lang['main_tojary_readmore'] 	= 'magbasa ng higit pa';
+
+$lang['main_prod_amaal_header']  = 'Amaal';
+$lang['main_prod_amaal_title']   = 'Propesyonal na Serbisyo ng Amaal';
+$lang['main_prod_amaal_content'] = 'Hindi lamang ang mga produkto ay maaaring pumatok sa online na merkato. Sa henerasyon ngayon karamihan sa mga mamimili ay umaasa na sa internet upang makuha ang serbisyo na kailangan nila para mapaluwag ang oras ng paglalakbay at pagsisikap. Bakit ka aasa sa print ads at nakasulat na anunsyo para maihayag ng iyong mga serbisyo . Hayaan Amaal na tumutulong sa iyo para mailagay ang mga servisyo mo online.
+
+Mula sa pasadyang disenyo at ng malawak na koleksyon ng mga tema , maaari mong piliin kung aling mga hitsura pinakamahusay na akma sa inyong negosyo. Sa Amaal maaari mong subaybayan at kontrolin ang iyong negosyo sa anumang oras at kahit saan ka man. Maaari kang mag-paskil ng mahusay na deal at itaguyod ang mga serbisyo sa hinaharap upang mapalakas ang iyong benta.';
+$lang['main_amaal_readmore'] 	= 'magbasa ng higit pa';
+
+//Training
+$lang['main_training_title']  = 'Libreng Pagsasanay';
+$lang['main_training_content']  = 'Nagbibigay kmi ng libreng pagsasanay para maibigay ang suportang kailangan upang epektibo ninyong magamit ang aming mga produkto.';
+$lang['main_training_facilities_heading'] = 'Pasilidad ng Pagsasanay';
+$lang['main_training_schedules_heading'] = 'Iskedyul ng Pagsasanay';
+
+//training schedules
+$lang['main_training_schedules_topic'] = 'Paksa';
+$lang['main_training_schedules_date'] = 'Petsa';
+$lang['main_training_schedules_time'] = 'Oras';
+$lang['main_training_schedules_facilitator'] = 'Namamahala';
+
+//Training booking form
+$lang['main_training_booking_form_heading'] = 'Listahan ng Paglalaan para sa Pagsasanay ';
+$lang['main_training_booking_form_name']	= 'Pangalan';
+$lang['main_training_booking_form_email']	= 'Email';
+$lang['main_training_booking_form_course']	= 'Kurso';
+$lang['main_training_booking_form_schedule']	= 'Iskedyul';
+
+
+//price list
+
+$lang['main_pricelist']				= 'Listahan ng Presyo';
+$lang['main_pricelist_aqary_header']= 'AQARY';
+$lang['main_pricelist_tojary_header']= 'TOJARY';
+$lang['main_pricelist_amaal_header'] = 'AMAAL';
+
+//knowledgebase
+
+$lang['main_knowledgebase'] = 'Base ng Kaalaman';
+$lang['main_aqary_knowledgebase'] = 'Base ng Kaalaman ng Aqary';
+$lang['main_tojary_knowledgebase'] = 'Base ng Kaalaman ng Tojary';
+$lang['main_amaal_knowledgebase'] = 'Base ng Kaalaman ng Amaal';
+$lang['main_aqary_knowledgebase_discrp'] = 'Lahat ng artikulo tungkol sa Aqary';
+$lang['main_tojary_knowledgebase_discrp'] = 'Lahat ng artikulo tungkol sa Tojary';
+$lang['main_amaal_knowledgebase_discrp'] = 'Lahat ng artikulo tungkol sa Amaal';
+$lang['main_articles_heading']		= 'Pinakasikat na Artikulo';
+
+
+
+
