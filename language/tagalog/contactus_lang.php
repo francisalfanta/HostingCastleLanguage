0037b8c5e2fb986ad53986ebf1247ebf06b7a25b
roxanne-2015 <info@investindubai.com>
2015-11-17 17:54:09 +0400

    Contact us

---
+++ language/tagalog/contactus_lang.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['contactus_title']	 = 'Makipagugnayan Samin';
+$lang['contact_addr_line1']  = 'Office No. 1806, 18th floor';
+$lang['contact_addr_line2']  = 'Marina Plaza, Dubai Marina, UAE';
+$lang['contact_phone'] 		 = 'Tel';
+$lang['contact_fax']		 = 'Fax';
+$lang['contact_email']		 = 'Email';
+$lang['office_timings_title']= 'Oras ng Opisina';
+$lang['contact_form_title']  = 'Contact Form';
+$lang['office_timings_date'] = '(Linggo) hanggang (Huwebes)';
+$lang['access_and_transport']= 'Pagpunta and Paglipat';
+$lang['directions']		 	 = 'Direksyon';
+$lang['downloadmap']		 = 'I-download ang Mapa';
+
+//Access and Transport details
+$lang['airport_title']		 = 'Paliparan';
+$lang['airport_desc']		 = 'Ang Marina Plaza Dubai ay 30 minuto mula sa Internasyonal na Paliparan ng Dubai at 60 minuto mula sa paliparan ng Abu Dhabi.';
+$lang['taxi_title']			 = 'Taksi at Kotse';
+$lang['taxi_decs']			 = 'Mula sa Dubai, gamitin ang tunnel sa Sheikh Zayed Road pagkatapos ng exit 32.';
+$lang['metro_title']		 = 'Metro at Bus';
+$lang['metro_desc']			 = 'Istasyon: JLT or Dubai Marina RTA Metro Shuttle Bus (#F37) humihinto mismo sa harap ng mall para sa magbaba at magsakay(ang mga bus ang dumadating kada 10 – 15 minuto).';
+$lang['parking_title']		 = 'Parking';
+$lang['parking_desc']		 = 'Karamihan ng mga gusali ay may pangkalahatang lugar ng paradahan sa kanilang paligid.';
+
+//directions
+
+$lang['direction_line1']	 = 'Mula sa JLT Metro Station hanggang Marina Plaza - 10 minuto (800m)';
+$lang['direction_line2']	 = 'Magtungo sa hilagang-kanluran ng JLT Metro Station patungo sa Al Ahaimer St.';
+$lang['direction_line3']	 = 'Bahagyang pakanan papunta sa Al Gharbi St.';
+$lang['direction_line4']	 = 'Bahagyang pakaliwa papunta sa Al Yahoom St.';
+$lang['direction_line5']     = 'Magpatuloy papunta sa Al Yahoom St.';
+$lang['direction_line6']     = 'Tumungo sa round about, ang destinasyon ay sa kanan.';
+
